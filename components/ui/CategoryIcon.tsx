d3a69ef2754875ef07b21fb9efd50afc96e013dc
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-11-03 20:07:00 -0500

    add: finalizar carga de productos y resaltar categoria actual(use client)

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,14 +1,21 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
 import { Category } from "@prisma/client"
+import { useParams } from "next/navigation"
 
 type CategoryIconProps = {
     category: Category
 }
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
+
+    const params = useParams<{ category: string }>()
+    if (category.slug === params.category) {
+        console.log("siii iguales")
+    }
     return (
-        <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 peer-last-of-type:border-b`}>
+        <div className={`${category.slug === params.category ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border-t border-gray-200 p-3 peer-last-of-type:border-b`}>
             <div className="w-16 h-16 relative">
                 <Image
                     fill
