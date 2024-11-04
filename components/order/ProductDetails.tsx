fd15238617f267e2125c853d152685c69d12ab26
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-11-04 16:50:10 -0500

    add: decrementar producto del pedido validando negativos

--- components/order/ProductDetails.tsx
+++ components/order/ProductDetails.tsx
@@ -2,14 +2,21 @@ import { MinusIcon, PlusIcon, XCircleIcon } from "@heroicons/react/20/solid"
 import { OrderItem } from "@/src/types"
 import { formatCurrency } from "@/src/utils"
 import { useStore } from "@/src/store"
+import { useMemo } from "react"
 
 type ProductDetailsProps = {
     item: OrderItem
 }
 
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
 export default function ProductDetails({ item }: ProductDetailsProps) {
 
     const increaseQuantity = useStore(state => state.increaseQuantity)
+    const decreaseQuantity = useStore(state => state.decreaseQuantity)
+    const disabledDecreaseButton = useMemo(() => item.quantity === MIN_ITEMS, [item])
+    const disabledIncreaseButton = useMemo(() => item.quantity === MAX_ITEMS, [item])
 
     return (
         <div className="shadow space-y-1 p-4 bg-white  border-t border-gray-200 ">
@@ -30,7 +37,9 @@ export default function ProductDetails({ item }: ProductDetailsProps) {
                 <div className="flex gap-5 px-10 py-2 bg-gray-100 w-fit rounded-lg">
                     <button
                         type="button"
-                        onClick={() => { }}
+                        onClick={() => decreaseQuantity(item.id)}
+                        disabled={disabledDecreaseButton}
+                        className="disabled:opacity-20"
                     >
                         <MinusIcon className="h-6 w-6" />
                     </button>
@@ -42,6 +51,8 @@ export default function ProductDetails({ item }: ProductDetailsProps) {
                     <button
                         type="button"
                         onClick={() => increaseQuantity(item.id)}
+                        disabled={disabledIncreaseButton}
+                        className="disabled:opacity-20"
                     >
                         <PlusIcon className="h-6 w-6" />
                     </button>
