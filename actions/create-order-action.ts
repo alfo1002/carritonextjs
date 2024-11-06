fdc8af27f2daa887a6380e71d2bc47936bce18a7
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-11-05 21:00:58 -0500

    add: validación de datos con ZOD desde el servidor

--- actions/create-order-action.ts
+++ actions/create-order-action.ts
@@ -1,5 +1,14 @@
 "use server"
 
-export async function createOrder() {
-    console.log('Creando orden con server action')
+import { OrderSchema } from "@/src/schema"
+
+export async function createOrder(data: unknown) {
+    const result = OrderSchema.safeParse(data)
+
+    if (!result.success) {
+        return {
+            errors: result.error.issues
+        }
+    }
+
 }
