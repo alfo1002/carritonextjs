152aaca4d207efb8d62dc31a5d558ca878503d2b
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-11-08 15:16:57 -0500

    add: añadir paginado por numeros

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -25,14 +25,13 @@ export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
 export default async function ProductsPage({ searchParams }: { searchParams: { page: string } }) {
 
     const page = +searchParams.page || 1
-    console.log(page)
     if (page < 0) redirect("/admin/products")
-
     const pageSize = 10
     const productsData = getProducts(page, pageSize)
     const totalProductsData = productCount()
     const [products, totalProducts] = await Promise.all([productsData, totalProductsData])
     const totalPages = Math.ceil(totalProducts / pageSize)
+    if (page > totalPages) redirect("/admin/products")
 
     return (
         <>
--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -6,6 +6,9 @@ type ProductsPaginationProps = {
 }
 
 export default function ProductsPagination({ page, totalPages }: ProductsPaginationProps) {
+
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1)
+
     return (
         <nav className="flex justify-center py-10">
 
@@ -18,6 +21,18 @@ export default function ProductsPagination({ page, totalPages }: ProductsPaginat
                         &laquo;
                     </Link>
                 )
+            }
+
+            {
+                pages.map(currentPage => (
+                    <Link
+                        key={currentPage}
+                        href={`/admin/products?page=${currentPage}`}
+                        className={`${page === currentPage && 'font-black'} bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-500 focus:z-20 focus:outline-offset-0`}
+                    >
+                        {currentPage}
+                    </Link>
+                ))
             }
 
             {page < totalPages && (
