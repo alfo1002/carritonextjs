5377a61e5db063d16303f538b29a4d8446b42cca
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-11-09 13:55:11 -0500

    add: obtener producto a edita y validar imagen

---
+++ app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,41 @@
+import EditProductForm from "@/components/products/EditProductForm"
+import ProductForm from "@/components/products/ProductForm"
+import GoBackButton from "@/components/ui/GoBackButton"
+import Heading from "@/components/ui/Heading"
+import { prisma } from "@/src/lib/prisma"
+import Link from "next/link"
+import { notFound } from "next/navigation"
+
+type EditProductPageProps = {
+    params: {
+        id: string
+    }
+}
+
+async function getProductById(id: number) {
+    const product = await prisma.product.findUnique({
+        where: {
+            id
+        }
+    })
+    if (!product) {
+        notFound()
+    }
+    return product
+}
+
+export default async function EditProductPage({ params }: EditProductPageProps) {
+    const product = await getProductById(+params.id)
+    return (
+        <>
+            <Heading>Editar Producto</Heading>
+
+            <GoBackButton />
+
+            <EditProductForm>
+                <ProductForm product={product} />
+            </EditProductForm>
+
+        </>
+    )
+}
